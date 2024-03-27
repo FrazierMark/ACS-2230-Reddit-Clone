7fb19a2d8cb108e9cd0bb3fc879a92af3c43507b
FrazierMark <m.mark.frazier@gmail.com>
2024-03-27 12:04:59 -0700

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,6 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
-	
-	app.get('/', async (req, res) => {
-		try {
-			const posts = await Post.find({}).lean();
-			return res.render('posts-index', { posts });
-		} catch (err) {
-			console.log(err.message);
-		}
-	});
-
 	// GET NEW Post Form
 	app.get('/posts/new', (req, res) => {
 		res.render('posts-new', {});
@@ -25,6 +15,25 @@ module.exports = (app) => {
 		} catch (error) {
 			console.error('Error saving post:', error);
 			res.status(500).send('Error saving post');
+		}
+	});
+
+	// SHOW
+	app.get('/posts/:id', async (req, res) => {
+		try {
+			const post = await Post.findById(req.params.id).lean();
+			res.render('posts-show', { post });
+		} catch (err) {
+			console.log(err.message);
+		}
+	});
+
+	app.get('/', async (req, res) => {
+		try {
+			const posts = await Post.find({}).lean();
+			return res.render('posts-index', { posts });
+		} catch (err) {
+			console.log(err.message);
 		}
 	});
 };
